7c25babcbb6aa06aaec42f651c3f8241ee341535
Igor Souto <igor_souto@outlook.com>
2020-01-07 04:01:36 -0300

    feat(post): add update post
    Add update post route, service and controller.

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {
   create,
   list,
+  update,
 } = require('../controllers/post')
 
 const router = express.Router()
@@ -9,5 +10,6 @@ const router = express.Router()
 router
   .post('/post', create)
   .get('/post', list)
+  .patch('/post/:id', update)
 
 module.exports = router
